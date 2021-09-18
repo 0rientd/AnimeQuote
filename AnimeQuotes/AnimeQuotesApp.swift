3f07c081dd5427c7cdc3931e4559932dc9af8824
dev.0rientd <chbarros.junior@gmail.com>
2021-09-18 00:50:15 -0300

    Initial Commit

---
+++ AnimeQuotes/AnimeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuotesApp.swift
+//  AnimeQuotes
+//
+//  Created by 0rientd on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
